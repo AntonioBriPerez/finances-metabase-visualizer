50910345b2b2b34962e65116910387f0285fab25
AntonioBriPerez <antoniobirrin@gmail.com>
2024-06-12 22:27:58 +0200

    ahora saca las ingresos brutos calculados, los ingresos netos, queda pendiente leer del directorio de nominas y convertir a excel los datos

---
+++ K_CONSTANTS.py
@@ -0,0 +1,18 @@
+keys = {
+    "salario_base": "SALARIO BASE",
+    "cesta": "CESTA",
+    "plus_convenio": "PLUS CONVENIO",
+    "mejora_absorbible": "MEJORA VOLUNTARIA ABSORBIBLE",
+    "base de cotizacion": "base_cotizacion",
+    "comp_teletrabajo": "COMPENSACION TELETRABAJO",
+    "p.extra.dic": "P.EXTRA DICIEM",
+    "p.extra.jul": "P.EXTRA JULIO",
+    "seguro_accidente": "SEGURO ACCIDENTE LABORAL",
+    "plus_seguro_medico_tributa": "PLUS SEGURO MEDICO - TRIBUTA",
+    "plus_seguro_medico_no_tributa": "PLUS SEGURO MEDICO SANITAS",
+    "intervenciones": "INTERVENCIONES",
+    "seguro_vida": "SEGURO VIDA",
+}
+
+date_pattern = r"\b(ene(?:ro)?|feb(?:rero)?|mar(?:zo)?|abr(?:il)?|may(?:o)?|jun(?:io)?|jul(?:io)?|ago(?:sto)?|sep(?:tiembre)?|oct(?:ubre)?|nov(?:iembre)?|dic(?:iembre)?)\s*[-\s]?\s*(19\d{2}|20\d{2})\b"
+number_pattern = r"[-+]?\d{1,3}(?:\.\d{3})*(?:,\d+)?(?:\.\d+)?"
